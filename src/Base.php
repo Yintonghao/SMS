662699074fda231e030540f8d336680322e31c6b
大菠萝 <@qq.com>
2023-10-06 14:12:45 +0800

    优化表结构

--- src/Base.php
+++ src/Base.php
@@ -16,15 +16,15 @@ class Base
     public $tableName = 'sms_setting';
     //表结构
     public $table = "CREATE TABLE `%s` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `type` int(6) NOT NULL DEFAULT '1' COMMENT '1-手机号  2-IP 3-UID',
-  `business` int(6) NOT NULL DEFAULT '1' COMMENT '业务ID',
-  `second` int(6) NOT NULL DEFAULT '1' COMMENT '发送频率，秒',
-  `num` int(6) NOT NULL DEFAULT '3' COMMENT '发送限制，次数',
+  `type` int(6) NOT NULL DEFAULT '0' COMMENT '1-手机号  2-IP 3-UID',
+  `business` int(6) NOT NULL DEFAULT '0' COMMENT '业务ID',
+  `second` int(6) NOT NULL DEFAULT '0' COMMENT '发送频率，秒',
+  `num` int(6) NOT NULL DEFAULT '0' COMMENT '发送限制，次数',
+  `sort` int(6) NOT NULL DEFAULT '0' COMMENT '排序',
   `create_time` int(11) DEFAULT NULL COMMENT '创建时间',
   `update_time` int(11) DEFAULT NULL COMMENT '更新时间',
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;";
+  PRIMARY KEY (`type`,`business`) USING BTREE
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='短信频发设置';";
     //枚举数据
     public $Enumdata = [];
     public $redisName = '';
