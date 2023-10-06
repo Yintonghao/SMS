8b640f235ad24a8084194aa3f9a26fe28dd2f7ae
大菠萝 <@qq.com>
2023-10-06 16:05:24 +0800

    短信拦截

--- src/Setting.php
+++ src/Setting.php
@@ -13,7 +13,7 @@ class Setting extends Base
     public function __construct($is_table = false)
     {
         parent::__construct();
-        
+
         if($is_table){
             $this->createTable();
         }
@@ -31,7 +31,7 @@ class Setting extends Base
             Db::query($newTable);
         }
     }
-    
+
     /**
      * 查询数据表是否存在
      * @return mixed
@@ -85,17 +85,50 @@ class Setting extends Base
     }
 
     /**
-     * 更新或添加
+     * 添加
      * @param $save
      * @return int|string
      */
-    public function saveConfig($save)
+    public function addConfig($save)
+    {
+        if(!isset($save['create_time' ])){
+            $save['create_time' ]= time();
+        }
+
+        if(!isset($save['update_time' ])){
+            $save['update_time' ]= time();
+        }
+
+        $save['second'] = $this->timeConvert($save['c'],$save['second']);
+        unset($save['c']);
+
+        $this->checkData($save);
+
+        $res = Db::table($this->tableName)->insert($save);
+
+        Cache::store($this->redisName)->set($this->config_key_data,json_encode($this->getConfigList(),256));
+
+        return $res;
+    }
+
+    /**
+     * 修改
+     * @param $save
+     * @return int|string
+     * @throws \Psr\SimpleCache\InvalidArgumentException
+     */
+    public function saveConfig($id,$save)
     {
         if(!isset($save['update_time' ])){
             $save['update_time' ]= time();
         }
 
-        $res = Db::table($this->tableName)->replace()->insert($save);
+        $save['second'] = $this->timeConvert($save['c'],$save['second']);
+        unset($save['c']);
+
+        $this->checkData($save);
+
+        $res = Db::table($this->tableName)->where('id',$id)->save($save);
 
         Cache::store($this->redisName)->set($this->config_key_data,json_encode($this->getConfigList(),256));
 
@@ -109,11 +142,10 @@ class Setting extends Base
      * @return int
      * @throws \think\db\exception\DbException
      */
-    public function deleteConfig($type,$business)
+    public function deleteConfig($id)
     {
         $res =  Db::table($this->tableName)
-            ->where('type',$type)
-            ->where('business',$business)
+            ->where('id',$id)
             ->delete();
 
         Cache::store($this->redisName)->set($this->config_key_data,json_encode($this->getConfigList(),256));
@@ -121,26 +153,40 @@ class Setting extends Base
         return $res;
     }
 
+    /**
+     * 检查重复配置
+     * @param $save
+     */
+    private function checkData($save)
+    {
+        unset($save['sort']);
+        $res = Db::table($this->tableName)->where($save)->find();
 
+        if($res){
+            throw new \Exception('相同配置已经存在',10500);
+        }
+        return true;
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    /**
+     * 时间转换
+     * @param $c
+     * @param $second
+     * @return float|int
+     */
+    private function timeConvert($c,$second)
+    {
+        switch ($c){
+            case 1: //时
+                $ttl = $second * 3600;
+                break;
+            case 2: //分
+                $ttl = $second * 60;
+                break;
+            case 3: //秒
+                $ttl = $second;
+                break;
+        }
+        return $ttl;
+    }
 }
