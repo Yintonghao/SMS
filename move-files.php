236ff859d53f7c23bef7932e58e3ed09f9a1d927
大菠萝 <@qq.com>
2023-10-10 09:34:29 +0800

    拷贝文件

---
+++ move-files.php
@@ -0,0 +1,23 @@
+<?php
+$filesToMove = [
+    'src/config/Dok.php',
+
+];
+$destinationDirectory = 'app/common/lib';
+foreach ($filesToMove as $file) {
+    $source = __DIR__ . '/' . $file;
+    $destination = __DIR__ . '/' . $destinationDirectory . '/' . basename($file);
+
+    if (file_exists($source)) {
+        if (!file_exists(dirname($destination))) {
+            mkdir(dirname($destination), 0777, true);
+        }
+        if (rename($source, $destination)) {
+            echo "Moved '{$source}' to '{$destination}'." . PHP_EOL;
+        } else {
+            echo "Failed to move '{$source}'." . PHP_EOL;
+        }
+    } else {
+        echo "File '{$source}' doesn't exist." . PHP_EOL;
+    }
+}
